e1955e9ec4d1946386603608085cb21da55c7441
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2022-12-22 14:09:10 +0100

    list__item--done class assigned to span instaed of li; reformated code for readability

--- src/List/index.js
+++ src/List/index.js
@@ -3,25 +3,26 @@ import "./style.css"
 const List = ({ tasks, doneTasksHidden }) => (
   <ul className="list">
     {tasks.map(task =>
-      <li key={task.id}
-        className={
-          `list__item${task.done & doneTasksHidden
-            ? " list__item--hidden list__item--done"
-            : task.done
-              ? " list__item--done"
-              : ""}`
-        }
+      <li
+        key={task.id}
+        className={`list__item${task.done & doneTasksHidden ? " list__item--hidden" : ""}`}
       >
-        <button title="toggle done"
-          className={`list__button${task.done ? " list__button--done" : ""}`}></button>
-        <span className="list__content">
+        <button
+          title="toggle done"
+          className={`list__button${task.done ? " list__button--done" : ""}`}
+        />
+        <span
+          className={`list__content${task.done ? " list__item--done" : ""}`}
+        >
           {task.content}
         </span>
-        <button title="delete task" className="list__button list__button--delete"></button>
+        <button
+          title="delete task"
+          className="list__button list__button--delete"
+        />
       </li>
     )}
   </ul >
 );
 
 export default List;
-
