839ba34b072465f5fe1ae98b7381eb4f62ad7c15
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-03 18:53:44 +0100

    theme added, GlobalStyle moved to index.js

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,18 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-}
+    html {
+        box-sizing: border-box;
+    }
 
-*,
-::after,
-::before {
-  box-sizing: inherit;
-}
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
 
-#root {
-  font-family: 'Montserrat', sans-serif;
-  background-color: #eee;
-}
-`;
+    #root {
+        font-family: 'Montserrat', sans-serif;
+        background-color: ${({ theme }) => theme.secondaryColor};
+    }
+    `;
+
+export const theme = {
+    primaryColor: '#fff',
+    secondaryColor: '#eee',
+    accentColor: 'hsl(187, 100%, 25%)',
+    accentHover: 'hsl(187, 100%, 30%)',
+    accentActive: 'hsl(187, 100%, 35%)'
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle, theme } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
