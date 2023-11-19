5d615acc0f71ef199fb1499702080f49dda68571
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-19 20:13:57 +0100

    added custom hooks to handle filtering query

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,18 @@
-import { useLocation, useHistory } from 'react-router-dom';
 import { Field } from "../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from '../searchQueryParamName';
+import { useQueryParameter} from "../queryParameters";
+import { useReplaceQueryParameter } from "../queryParameters";
 
 export default () => {
-    const history = useHistory();
-    const location = useLocation();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
     };
 
     return (
