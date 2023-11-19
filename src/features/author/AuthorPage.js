337cab6fb4aa4b1a53a6f9f7869023063c7e6aad
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-19 20:13:14 +0100

    added github link to authors page

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -12,10 +12,24 @@ const Author = () => (
       body={
         <>
           <p>
-            W ramach kryzusu przed czerdziestką, zamiast kupić Teslę postanowiłem zmienić zawód, żeby może kiedyś odłożyć na większe mieszkanie.
+            W ramach kryzusu przed czerdziestką,
+            zamiast kupić Teslę postanowiłem zmienić zawód,
+            żeby może kiedyś odłożyć na większe mieszkanie.
           </p>
           <p>
-            A tak na serio chciałbym spróbować czegoś nowego i powiększyć swój wachlarz umiejętności. Programowanie wydało mi się ciekawe, myślę, że mogę się w tym odnaleść i mieć z tego frajdę
+            A tak na serio chciałbym spróbować czegoś nowego i
+            powiększyć swój wachlarz umiejętności.
+            Programowanie wydało mi się ciekawe, myślę,
+            że mogę się w tym odnaleść i mieć z tego frajdę
+          </p>
+          <p>
+            Zapraszam na mój profil na githubie gdzie znajdziesz kod tej aplikacji i pozsotałe projekty:<br />
+            <a
+              href="https://github.com/bedicooper/"
+              target='blank'
+            >
+              github.com/bedicooper/
+            </a>
           </p>
         </>
       }
